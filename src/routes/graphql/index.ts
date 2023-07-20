ec0715a6a1388ff84a01d98869e28258bee62938
webbomj <nethack@mail.ru>
2023-07-20 19:18:43 +0300

    feat: add depthlimit

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,10 +1,12 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { GraphQLSchema, graphql } from 'graphql';
+import { GraphQLSchema, graphql, validate, parse } from 'graphql';
 import { query as querySchema } from './query/query.js';
 import { mutation } from './query/mutation.js';
+import depthLimit from 'graphql-depth-limit'
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
+  
   fastify.route({
     url: '/',
     method: 'POST',
@@ -14,14 +16,22 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         200: gqlResponseSchema,
       },
       
-
     },
-    async handler(req) {
+    
+    
+    async handler(req, reply) {
       const { query, variables} = req.body
       const schema = new GraphQLSchema({
         query: querySchema,
         mutation: mutation
       })
+
+      const errors = validate(schema, parse(String(query)), [depthLimit(5)])
+      if (errors.length > 0) {
+				reply.send({ data: null, errors: errors });
+				return;
+			}
+
       return await graphql({
         schema: schema,
         source: String(query),
